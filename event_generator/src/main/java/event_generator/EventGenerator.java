d0ca4ae20d91d7450898801181babcd877c17dce
AltPhilipp <philipp.altenbach@students.fhnw.ch>
2024-10-31 16:02:02 +0100

    Testing

--- event_generator/src/main/java/event_generator/EventGenerator.java
+++ event_generator/src/main/java/event_generator/EventGenerator.java
@@ -7,7 +7,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-
+// Test
 public class EventGenerator {
 
 	public static void main(String[] args) {
