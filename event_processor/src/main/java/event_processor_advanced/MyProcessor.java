997dfa5e4493fc1816a1a6f524f402a85594a773
ryan1480 <44569050+ryan1480@users.noreply.github.com>
2024-11-01 12:27:24 +0100

    Update MyProcessor.java

--- event_processor/src/main/java/event_processor_advanced/MyProcessor.java
+++ event_processor/src/main/java/event_processor_advanced/MyProcessor.java
@@ -4,12 +4,11 @@ import org.apache.kafka.streams.kstream.ForeachAction;
 import org.apache.kafka.streams.kstream.Windowed;
 
 
-/* Just a silly example */
 public class MyProcessor implements ForeachAction<Windowed<String>, Double>{
 
 	@Override
 	public void apply(Windowed<String> key, Double value) {
-		System.out.println("MyProcessor::got message  K:"+key+"   V:"+value);
+		System.out.println("Added event to the Dashboard: Large Order requiring big box; Products:"+key+"; Average order quantity:"+value);
 	}
 
 }
